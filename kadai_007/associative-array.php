e43bcf03532ef730bf66e59525cf15f16d8c9a10
kinkin0201 <129183601+kinkin0201@users.noreply.github.com>
2024-10-13 16:38:07 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,11 @@
+<?php
+$student = [
+    "name" => "Taro",
+    "age" => 20,
+    "grade" => "A"
+];
+
+echo "Name: " . $student["name"] . "<br>";
+echo "Age: " . $student["age"] . "<br>";
+echo "Grade: " . $student["grade"] . "<br>";
+?>
